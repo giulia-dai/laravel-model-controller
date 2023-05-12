236a4192c6f424df7813d63c5122d9f3580c4c54
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-05-12 10:47:49 +0200

    piccole modifiche correzione mattina

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,11 @@ class PageController extends Controller
 {
     public function index()
     {
+        // filtra tutti i film
         $movies = Movie::all();
+
+        // filtra solo i film con voto > 9
+        //$movies = Movie::where('vote', '>', 9)->get();
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,9 @@
         <i class="fa-solid fa-circle-down"></i>
     </h4>
 
-    @foreach ($movies as $movie)
+
+    {{-- va bene usare il foreach ma il forelse considera naturalmente il caso in cui non ci sia niente da ciclare con il campo empty --}}
+    @forelse ($movies as $movie)
         <div class="col-3 ">
             <div class="card ms_card">
                 <div class="card-body">
@@ -22,6 +24,26 @@
                 </div>
             </div>
         </div>
-    @endforeach
+    @empty
+        <div class="alert alert-danger">
+            Nessun risultato
+        </div>
+    @endforelse
+
+    {{-- @foreach ($movies as $movie)
+        <div class="col-3 ">
+            <div class="card ms_card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{ $movie->original_title }}</h6>
+                    <div>Nationality: {{ $movie->nationality }} </div>
+                    <span class="year"> Year {{ $movie->date }}</span>
+                    <h6><i class="fa-solid fa-star"></i>Vote:
+                        {{ $movie->vote }}
+                    </h6>
+                </div>
+            </div>
+        </div>
+    @endforeach --}}
 
 @endsection
